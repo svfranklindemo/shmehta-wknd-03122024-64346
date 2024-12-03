723a02a32f776a7caa71f2f8e98502abe06b046e
svfranklindemo <166366015+svfranklindemo@users.noreply.github.com>
2024-12-03 14:28:40 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,3 +3,15 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('lpl-eds');
 
 // add more delayed functionality here
+
+// Select the navbar
+const navbar = document.querySelector('.nav-wrapper');
+
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) { // Adjust the scroll threshold as needed
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
